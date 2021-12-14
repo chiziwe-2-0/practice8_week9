3142df4acaa2a7af51ec31340956eea10b93ee06
Nikita Olimpiev <nikitaolimpiev97@gmail.com>
2021-12-15 02:28:35 +0300

    mongo

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ import http from 'http'
 import zombie from 'zombie'
 
 
-
 import appSrc from './app.js';
 
-const app = appSrc(fs, express, crypto, http, zombie);
+const app = appSrc(fs, express, crypto, http, zombie, MongoClient);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
