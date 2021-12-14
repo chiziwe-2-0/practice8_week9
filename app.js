da10c126a8a6e25bc6a94fe6d05cc12f3cea991b
Nikita Olimpiev <nikitaolimpiev97@gmail.com>
2021-12-15 01:55:06 +0300

    week 6

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-export default function appSrc(express, zombie) {
+export default function appSrc(fs, express, crypto, http, zombie) {
     const app = express();
 
     app
         .use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,OPTIONS,DELETE");
-        next();
+          res.header("charset", "utf-8");
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,OPTIONS,DELETE");
+          next();
       })
 
-
         .use('/login/', (req, res) => res.send('itmo307702'))
+
+        .all('/code', async (req, res) => {
+          let result = '';
+          const reader = fs.createReadStream(import.meta.url.substring(7));
+          for await (const chunk of reader) result += chunk;
+          res.send(result);
+        })
+
+        .all('/sha1/:input/', (req, res) => {
+          res.send(crypto.createHash('sha1').update(req.params.input).digest('hex'));
+        })
+
+        .all('/req', (req, res) => {
+          let addr = req.body.addr;
+          let data = ''
+          if (url)
+              http.get(addr, {headers: {'Content-Type': 'text/plain'}}, response => {
+                  response.setEncoding('utf8')
+                  response.on('data', chunk => data += chunk)
+                  response.on('end', () => res.send(data))
+              })
+          else
+              res.send('Данных нет!')
+        })
 
         .use('/test/', async(req, res) => {
             const page = new zombie();
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-import express from 'express';
-import zombie from 'zombie';
+import express from 'express'
+import fs from 'fs'
+import crypto from 'crypto'
+import http from 'http'
+import zombie from 'zombie'
+
+
 
 import appSrc from './app.js';
 
-const app = appSrc(express, zombie);
-
+const app = appSrc(fs, express, crypto, http, zombie);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('Старт!');
+  console.log('Стартуем!');
 });
